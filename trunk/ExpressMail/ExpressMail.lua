2e7fa246bff4a3dea4347a7fe1378662c1108b6f
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2006-12-14 10:55:26 +0000

    ExpressMail - Removed text check, should stop mail from being "opened" and subject to 3-day expiration

--- trunk/ExpressMail/ExpressMail.lua
+++ trunk/ExpressMail/ExpressMail.lua
@@ -2,9 +2,7 @@
 
 local gttsiiHook = function(tooltip, index, ...)
 	local _, _, _, _, money, COD, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(index)
-	local bodyText, _, isTakeable = GetInboxText(index)
-	if money > 0 or COD > 0 or hasItem or isTakeable then tooltip:AddLine("Shift - Take Item") end
---~ 	else tooltip:AddLine("Shift - Delete Mail") end
+	if money > 0 or COD > 0 or hasItem then tooltip:AddLine("Shift - Take Item") end
 
 	if not wasReturned and canReply then tooltip:AddLine("Ctrl - Return Item") end
 
@@ -23,7 +21,6 @@ InboxFrame_OnClick = function(index, ...)
 		if money > 0 then TakeInboxMoney(index)
 		elseif COD > 0 then return
 		elseif hasitem then TakeInboxItem(index) end
---~ 		else DeleteInboxItem(index) end
 	elseif IsControlKeyDown() then
 		if wasReturned or not canReply then return end
 		ReturnInboxItem(index)
