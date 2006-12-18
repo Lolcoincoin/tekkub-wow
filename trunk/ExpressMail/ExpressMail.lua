3ce9221bb6193b8ac93ac5d554d1230a55e0f7a1
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2006-12-18 23:40:15 +0000

    ExpressMail - Fucking typos

--- trunk/ExpressMail/ExpressMail.lua
+++ trunk/ExpressMail/ExpressMail.lua
@@ -3,9 +3,7 @@
 local gttsiiHook = function(tooltip, index, ...)
 	local _, _, _, _, money, COD, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(index)
 	if money > 0 or COD > 0 or hasItem then tooltip:AddLine("Shift - Take Item") end
-
 	if not wasReturned and canReply then tooltip:AddLine("Ctrl - Return Item") end
-
 	return ...
 end
 local gttsii = GameTooltip.SetInboxItem
@@ -20,10 +18,8 @@ InboxFrame_OnClick = function(index, ...)
  	if IsShiftKeyDown() then
 		if money > 0 then TakeInboxMoney(index)
 		elseif COD > 0 then return
-		elseif hasitem then TakeInboxItem(index) end
-	elseif IsControlKeyDown() then
-		if wasReturned or not canReply then return end
-		ReturnInboxItem(index)
+		elseif hasItem then TakeInboxItem(index) end
+	elseif IsControlKeyDown() and not wasReturned and canReply then ReturnInboxItem(index)
 	else ifoc(index, ...) end
 end
 
