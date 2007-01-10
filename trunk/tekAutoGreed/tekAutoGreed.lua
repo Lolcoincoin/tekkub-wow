2a8886888445f6e6564d90a1ead745a3dad9114c
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-01-10 09:37:08 +0000

    tekAutoGreed - Should work now, but it's untested.

--- trunk/tekAutoGreed/tekAutoGreed.lua
+++ trunk/tekAutoGreed/tekAutoGreed.lua
@@ -1,25 +1,26 @@
-tekAutoGreed = {
-	duos = {
-		{"Icecrown", "Tekkub", "Rohona"},
-	}	,
+local duos = {
+	{"Icecrown", "Tekkub", "Rohona"},
 }
+
+
+local function InDuo()
+	if UnitExists("party2") or UnitExists("raid1") then return end
+
+	for _,duo in pairs(duos) do
+		if (GetRealmName() == duo[1] and ((UnitName("player") == duo[2] and UnitName("party1") == duo[3])
+		or (UnitName("player") == duo[3] and UnitName("party1") == duo[2]))) then
+			return true end
+	end
+end
+
 
 local orig = GroupLootFrame_OnShow
 GroupLootFrame_OnShow = function (...)
-	if not select(5, GetLootRollItemInfo(this.rollID)) and TreyIsGreedy:InDuo() then
+	if not select(5, GetLootRollItemInfo(this.rollID)) and InDuo() then
 		RollOnLoot(this.rollID, 2)
 	end
 	return orig(...)
 end
 
 
-function TreyIsGreedy:InDuo()
-	if UnitExists("party2") or UnitExists("raid1") then return end
-
-	for _,duo in pairs(self.duos) do
-		if (GetRealmName() == duo[1] and ((UnitName("player") == duo[2] and UnitName("party1") == duo[3])
-		or (UnitName("player") == duo[3] and UnitName("party1") == duo[2]))) then
-			return true end
-	end
-end
 
