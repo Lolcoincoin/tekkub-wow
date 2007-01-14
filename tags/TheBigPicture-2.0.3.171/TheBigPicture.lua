42a0a7412936c7ebd590bd999456167a0a57ab08
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-01-14 20:44:44 +0000

    Tagging TheBigPicture-2.0.3.171

---
+++ tags/TheBigPicture-2.0.3.171/TheBigPicture.lua
@@ -0,0 +1,39 @@
+﻿
+
+local metro = DongleStub("MetrognomeNano-Beta0")
+
+TheBigPicture = DongleStub("Dongle-Beta0"):New("TheBigPicture")
+
+
+local orig = Minimap.SetZoom
+local scrolldowns = {}
+local delay = 20  -- Change this value if you want a different delay between your last scroll
+	                -- and the time the frame resets.  This value is in seconds.
+
+function TheBigPicture:Initialize()
+	metro:Register(self, "TheBigPicture Timeout", "ResetFrame", delay)
+	metro:Register(self, "TheBigPicture Tick", "ScrollOnce", 0.2)
+	Minimap.SetZoom = function(...)
+		metro:Stop("TheBigPicture Tick")
+		metro:Start("TheBigPicture Timeout")
+		orig(...)
+	end
+end
+
+
+function TheBigPicture:ResetFrame(elapsed)
+	metro:Stop("TheBigPicture Timeout")
+	metro:Start("TheBigPicture Tick")
+end
+
+
+function TheBigPicture:ScrollOnce(elapsed)
+	local z = Minimap:GetZoom()
+	if z <= 0 then
+		metro:Stop("TheBigPicture Tick")
+		MinimapZoomIn:Enable()
+		MinimapZoomOut:Disable()
+	else orig(Minimap, z - 1) end
+end
+
+
