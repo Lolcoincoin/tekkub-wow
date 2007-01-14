574498404ad716230cc66db1f5c19ae1783c768f
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-01-14 20:45:38 +0000

    Tagging TheLowDown-2.0.3.172

---
+++ tags/TheLowDown-2.0.3.172/TheLowDown.lua
@@ -0,0 +1,44 @@
+﻿
+
+local metro = DongleStub("MetrognomeNano-Beta0")
+
+TheLowDown = DongleStub("Dongle-Beta0"):New("TheLowDown")
+
+
+local scrolldowns = {}
+local delay = 20  -- Change this value if you want a different delay between your last scroll
+	                -- and the time the frame resets.  This value is in seconds.
+
+function TheLowDown:Initialize()
+	local _G = getfenv(0)
+	local funcs = {"ScrollUp", "ScrollDown", "ScrollToTop", "PageUp", "PageDown"}
+
+	for i=1,7 do
+		local name = "ChatFrame" .. i
+		local frame = _G[name]
+		scrolldowns[name] = frame.ScrollDown
+		metro:Register(self, name.."DownTick", "ScrollOnce", 0.1, name, frame)
+		metro:Register(self, name.."DownTimeout", "ResetFrame", delay, name, frame)
+		for _,func in ipairs(funcs) do
+			local orig = frame[func]
+			frame[func] = function(...)
+				metro:Stop(name.."DownTick")
+				metro:Start(name.."DownTimeout", 1)
+				orig(...)
+			end
+		end
+	end
+end
+
+
+function TheLowDown:ResetFrame(elapsed, name, frame)
+	metro:Stop(name.."DownTimeout")
+	metro:Start(name.."DownTick")
+end
+
+
+function TheLowDown:ScrollOnce(elapsed, name, frame)
+	if frame:AtBottom() then metro:Stop(name.."DownTick")
+	else scrolldowns[name](frame) end
+end
+
