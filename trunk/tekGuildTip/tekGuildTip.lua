75f65efd928ba237ece3c367e0288d452da7be88
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-01-17 12:08:44 +0000

    tekGuildTip - Updated to new OnTooltipSetUnit secksores, no longer needs Dongle!

--- trunk/tekGuildTip/tekGuildTip.lua
+++ trunk/tekGuildTip/tekGuildTip.lua
@@ -1,79 +1,40 @@
 
-local gtt = GameTooltip
-local l1, l2, l3, l4 = GameTooltipTextLeft1, GameTooltipTextLeft2, GameTooltipTextLeft3, GameTooltipTextLeft4
+local GameTooltip, GameTooltipTextLeft1 = GameTooltip, GameTooltipTextLeft1
+local GameTooltipTextLeft2, GameTooltipTextLeft3 = GameTooltipTextLeft2, GameTooltipTextLeft3
 
 
-tekGuildTip = DongleStub("Dongle-Beta0"):New("tekGuildTip")
+local orig = GameTooltip:GetScript("OnTooltipSetUnit")
+GameTooltip:SetScript("OnTooltipSetUnit", function(...)
+	if GameTooltip:IsUnit("mouseover") then
+		local unit = "mouseover"
+		if not UnitExists(unit) or not UnitPlayerControlled(unit) then return end
 
+		local guildName = GetGuildInfo(unit)
+		if guildName then
+			local line2orig = GameTooltipTextLeft2:GetText()
+			if not line2orig or string.byte(line2orig) == 60 then return end
 
-function tekGuildTip:Enable()
-	self:RegisterEvent("UPDATE_MOUSEOVER_UNIT")
-end
-
-
-function tekGuildTip:UPDATE_MOUSEOVER_UNIT()
-	self:ModifyTooltip("mouseover")
-end
-
-
-function tekGuildTip:ModifyTooltip(unit)
-	if not UnitExists(unit) or not UnitPlayerControlled(unit) then return end
-
-	local guildName = GetGuildInfo(unit)
-	if guildName then
-		local line2orig = l2:GetText()
-		if not line2orig or string.byte(line2orig) == 60 then return end
-
-		if gtt:NumLines() == 3 then
-			local line3orig = l3:GetText()
-			l2:SetText("<"..guildName..">")
-			l3:SetText(line2orig)
-			gtt:AddLine(line3orig, 1.0, 1.0, 1.0)
-		else
-			l2:SetText("<"..guildName..">")
-			gtt:AddLine(line2orig, 1.0, 1.0, 1.0)
+			if GameTooltip:NumLines() == 3 then
+				local line3orig = GameTooltipTextLeft3:GetText()
+				GameTooltipTextLeft2:SetText(string.join("", "<", guildName, ">"))
+				GameTooltipTextLeft3:SetText(line2orig)
+				GameTooltip:AddLine(line3orig, 1.0, 1.0, 1.0)
+			else
+				GameTooltipTextLeft2:SetText(string.join("", "<", guildName, ">"))
+				GameTooltip:AddLine(line2orig, 1.0, 1.0, 1.0)
+			end
+		end
+		if UnitIsAFK(unit) then
+			local lineText = GameTooltipTextLeft1:GetText()
+			if lineText and not string.find(lineText, CHAT_FLAG_AFK) then
+				GameTooltipTextLeft1:SetText(string.join(" ", lineText, CHAT_FLAG_AFK))
+			end
+		elseif UnitIsDND(unit) then
+			local lineText = GameTooltipTextLeft1:GetText()
+			if lineText and not string.find(lineText, CHAT_FLAG_DND) then
+				GameTooltipTextLeft1:SetText(string.join(" ", lineText, CHAT_FLAG_DND))
+			end
 		end
 	end
-	if UnitIsAFK(unit) then
-		local lineText = l1:GetText()
-		if lineText and not string.find(lineText, CHAT_FLAG_AFK) then
-			l1:SetText(lineText.." "..CHAT_FLAG_AFK)
-		end
-	elseif UnitIsDND(unit) then
-		local lineText = l1:GetText()
-		if lineText and not string.find(lineText, CHAT_FLAG_DND) then
-			l1:SetText(lineText.." "..CHAT_FLAG_DND)
-		end
-	end
-	gtt:Show()
-end
-
-
----------------------
---      Hooks      --
----------------------
-
-local orig1 = GameTooltip.SetUnit
-local function posthook(unit, ...)
-	tekGuildTip:ModifyTooltip(unit)
-	return ...
-end
-GameTooltip.SetUnit = function(self, unit, ...)
-	return posthook(unit, orig1(self, unit, ...))
-end
-
-
-local orig2 = GameTooltip_OnHide
-GameTooltip_OnHide = function(...)
-	local line2orig = l2:GetText()
-	if line2orig and string.byte(line2orig) == 60 then
-		-- We added a line, we need to remove it or it'll show up in all future tooltips
-		l4:SetText()
-		l4:Hide()
-		l3:SetText()
-		l3:Hide()
-	end
-	return orig2(...)
-end
-
-
+	if orig then return orig(...) end
+end)
