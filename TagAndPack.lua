d261a5be01c631e7489a9eee891384a7c362df11
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-02-05 22:36:33 +0000

    Build script update

--- TagAndPack.lua
+++ TagAndPack.lua
@@ -1,20 +1,23 @@
 
-local addon = arg[1]
-local toc = arg[2]
-local rev = arg[3]
 local svnpath = "https://tekkub-wow.googlecode.com/svn"
-local svn = '"C:\\Program Files\\SVN\\bin\\svn"'
-local winrar = '"C:\\Program Files\\WinRAR\\rar"'
+
+
+io.write("Tag name (Addon-T.O.C.rev): ")
+local version = io.read()
+
+local _, _, addon, toc, rev = string.find(version, "^(.+)-(%d+%.%d+%.%d+)%.(%d+)$")
+assert(addon and toc and rev, "Invalid input!")
+
 local fulladdon = string.format("%s-%s.%s", addon, toc, rev)
 
 
 os.execute(string.format(
-	'%s copy -r %s %s/trunk/%s %s/tags/%s -m "Tagging %s"',
-	svn, rev, svnpath, addon, svnpath, fulladdon, fulladdon))
+	"svn copy -r %s %s/trunk/%s %s/tags/%s -m \"Tagging %s\"",
+	rev, svnpath, addon, svnpath, fulladdon, fulladdon))
 
-os.execute(string.format("%s export trunk/%s %s", svn, addon, addon))
+os.execute(string.format("svn export trunk/%s %s", addon, addon))
 
-os.execute(string.format("%s a %s.zip %s", winrar, fulladdon, addon))
+os.execute(string.format("rar a %s.zip %s", fulladdon, addon))
 
 os.execute("del /F /S /Q "..addon)
 os.execute("rmdir /Q "..addon)
