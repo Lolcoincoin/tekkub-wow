ca3b9c06364588c1951775fa9648878d03e7f75b
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-05-15 14:09:39 +0000

    tekNoDual - Better message, no friend check.

--- trunk/tekNoDual/tekNoDual.lua
+++ trunk/tekNoDual/tekNoDual.lua
@@ -1,35 +1,10 @@
 
-
-local function IsFriend(name)
-	for i=1,GetNumFriends() do
-		if GetFriendInfo(i) == name then return true end
-	end
-
-	if not IsInGuild() then return end
-
-	for i=1,GetNumGuildMembers() do
-		if GetGuildRosterInfo(i) == name then return true end
-	end
-end
-
-
-local function CancelDual(frame, event, unit)
-	if IsFriend(unit) then return end
-
+local f, warned = CreateFrame("Frame"), {}
+f:RegisterEvent("DUEL_REQUESTED")
+f:SetScript("OnEvent", function(self, event, name)
 	HideUIPanel(StaticPopup1)
 	CancelDuel()
-	SendChatMessage("[tekNoDual]: This player does not accept random duals.", "WHISPER", GetDefaultLanguage("player"), unit)
-end
-
-
-local f = CreateFrame("Frame")
-f:RegisterEvent("PLAYER_LOGIN")
-f:SetScript("OnEvent", function(frame)
-	if IsInGuild() then GuildRoster() end
-	if GetNumFriends() > 0 then ShowFriends() end
-
-	frame:UnregisterEvent("PLAYER_LOGIN")
-	frame:RegisterEvent("DUEL_REQUESTED")
-	frame:SetScript("OnEvent", CancelDual)
+	if not warned[name] then SendChatMessage("[tekNoDuel]: Bugger off sod!  Don't you have a flag to go steal?", "WHISPER", GetDefaultLanguage("player"), name) end
+	warned[name] = true
 end)
 
