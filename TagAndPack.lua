345964ac1bf66e5b4deb71771297beb6efd1515f
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-05-29 02:57:17 +0000

    Testing CIA....

--- TagAndPack.lua
+++ TagAndPack.lua
@@ -76,3 +76,4 @@ print("Packaging release")
 os.execute(string.format("svn export %s/tags/%s %s", svnpath, fulladdon, addon))
 os.execute(string.format("\"c:\\Program Files\\WinRAR\\WinRAR.exe\" a %s.zip %s", fulladdon, addon))
 os.execute("rmdir /Q /S "..addon)
+
