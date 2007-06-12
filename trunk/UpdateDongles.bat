0e429cf8e9ffdc077544036ec0068fce4794ba86
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-06-12 01:57:36 +0000

    Updating embeds to LegoBlock r29

--- trunk/UpdateDongles.bat
+++ trunk/UpdateDongles.bat
@@ -4,10 +4,14 @@ IF NOT EXIST Dongle.lua GOTO utils
 FOR /D %%j in (*) do IF EXIST %%j\Dongle.lua copy /Y Dongle.lua %%j
 
 :utils
-IF NOT EXIST DongleUtils.lua GOTO theend
+IF NOT EXIST DongleUtils.lua GOTO legos
 FOR /D %%j in (*) do IF EXIST %%j\DongleUtils.lua copy /Y DongleUtils.lua %%j
 
+:legos
+IF NOT EXIST LegoBlock.lua GOTO theend
+FOR /D %%j in (*) do IF EXIST %%j\LegoBlock.lua copy /Y LegoBlock.lua %%j
 
 :theend
 DEL Dongle.lua
 DEL DongleUtils.lua
+DEL LegoBlock.lua 
