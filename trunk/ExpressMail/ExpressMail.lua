9032b09eea138d735788c66a8357363a307fdca8
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-06-25 07:48:27 +0000

    ExpressMail - Added "Super express mode" checkbox.  Tick this and you don't have to hold down shift to take items on click

--- trunk/ExpressMail/ExpressMail.lua
+++ trunk/ExpressMail/ExpressMail.lua
@@ -1,4 +1,6 @@
 
+local stickyclick
+
 
 local gttsiiHook = function(tooltip, index, ...)
 	local _, _, _, _, money, COD, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(index)
@@ -15,7 +17,7 @@ end
 local ifoc = InboxFrame_OnClick
 InboxFrame_OnClick = function(index, ...)
 	local _, _, _, _, money, COD, _, hasItem, _, wasReturned, _, canReply = GetInboxHeaderInfo(index)
- 	if IsShiftKeyDown() then
+ 	if IsShiftKeyDown() or stickyclick then
 		if money > 0 then TakeInboxMoney(index)
 		elseif COD > 0 then return
 		elseif hasItem then TakeInboxItem(index) end
@@ -24,3 +26,15 @@ InboxFrame_OnClick = function(index, ...)
 end
 
 
+local sticky = CreateFrame("CheckButton", "ExpressMailCheck", InboxFrame, "OptionsCheckButtonTemplate")
+sticky:SetWidth(22)
+sticky:SetHeight(22)
+sticky:SetPoint("TOPLEFT", 80, -50)
+local stickylabel = sticky:CreateFontString(nil, "ARTWORK", "GameFontNormalSmall")
+stickylabel:SetPoint("LEFT", sticky, "RIGHT", 5, 0)
+stickylabel:SetText("Super express mode")
+sticky:SetScript("OnClick", function() stickyclick = not stickyclick end)
+sticky:SetScript("OnHide", function(self)
+	stickyclick = false
+	self:SetChecked(false)
+end)
