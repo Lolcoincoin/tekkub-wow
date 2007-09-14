cddf82b31d5fcd4336759306f33ae46f8d61b731
tekkub <tekkub@86fe6d9a-1522-0410-a387-bf9db416f0a0>
2007-09-14 00:47:10 +0000

    Could it be? WidgetWarlock is a real lib? NEVAR!
    Totally untested, FYI, just getting it up in the SVN so I can start hacking at it.

---
+++ trunk/WidgetWarlock/ArtSummons.lua
@@ -0,0 +1,60 @@
+
+local lib, oldminor = LibStub:NewLibrary("WidgetWarlock-Alpha1", 4)
+if not lib then return end
+
+
+
+-- Creates a texture object based on coords.
+-- Cannot SetPoint!  You must do this.
+-- Parent and coords (...) are required, all others optional
+function lib:SummonTextureWithCoords(parent, layer, w, h, texture, ...)
+	assert(parent, "Must pass a parent frame to create the texture")
+
+	local tex = parent:CreateTexture(nil, layer)
+	tex:SetTexture(texture)
+	tex:SetTexCoord(...)
+	if w then tex:SetWidth(w) end
+	if h then tex:SetHeight(h) end
+	return tex
+end
+
+
+-- Create a texture, set dims, texture and point if passed
+-- Parent required, all others optional
+function lib:SummonTexture(parent, layer, w, h, texture, ...)
+	assert(parent, "Must pass a parent frame to create the texture")
+
+	local tex = parent:CreateTexture(nil, layer)
+	if w then tex:SetWidth(w) end
+	if h then tex:SetHeight(h) end
+	tex:SetTexture(texture)
+	if select(1, ...) then tex:SetPoint(...) end
+	return tex
+end
+
+
+-- Create a font string, set it's text and point
+-- Parent required, all others optional
+function lib:SummonFontString(parent, layer, inherit, text, ...)
+	assert(parent, "Must pass a parent frame to create the fontstring")
+
+	local fs = parent:CreateFontString(nil, layer, inherit)
+	fs:SetText(text)
+	if select(1, ...) then fs:SetPoint(...) end
+	return fs
+end
+
+
+-- Attaches a label to a frame
+-- Parent required, all others optional
+-- Defaults to right side of the frame is no point is given
+function lib:EnslaveLabel(parent, text, a1, aframe, a2, dx, dy)
+	assert(parent, "Must pass a parent frame to create the fontstring")
+
+	if not a1 then a1, aframe, a2, dx, dy = "LEFT", parent, "RIGHT", 5, 0 end
+	local fs = parent:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
+	fs:SetPoint(a1, aframe, a2, dx, dy)
+	fs:SetText(text)
+	return fs
+end
+
---
+++ trunk/WidgetWarlock/Tooltips.lua
@@ -0,0 +1,32 @@
+
+local lib, oldminor = LibStub:NewLibrary("WidgetWarlock-Alpha1", 3)
+if not lib then return end
+
+
+
+local tipvalues, tipanchors = {}, {}
+local GameTooltip = GameTooltip
+
+local function HideTooltip() GameTooltip:Hide() end
+
+
+local function ShowTooltip(self)
+	local text = type(tipvalues[self]) == "function" and tipvalues[self]() or tipvalues[self]
+	GameTooltip:SetOwner(self, tipanchors[self])
+	GameTooltip:SetText(text)
+end
+
+
+function lib:EnslaveTooltip(frame, text, anchor)
+	assert(frame, "Must pass a frame")
+
+	if not text then
+		frame:SetScript("OnEnter", nil)
+		frame:SetScript("OnLeave", nil)
+	else
+		frame:SetScript("OnEnter", ShowTooltip)
+		frame:SetScript("OnLeave", HideTooltip)
+		tipvalues[frame] = text
+		tipanchors[frame] = anchor or "ANCHOR_RIGHT"
+	end
+end
